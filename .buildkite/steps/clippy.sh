386f2422570a9e66c8824593351d5e5505530e10
Chris Atkins <chris.a@buildkite.com>
2026-02-18 21:12:00 +1100

    Add Buildkite CI pipeline aligned with slatedb-rb

---
+++ .buildkite/steps/clippy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "--- :debian: Installing dependencies"
+apt-get update && apt-get install -y libclang-dev
+
+echo "--- :rust: Installing Rust"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source ~/.cargo/env
+
+echo "+++ :rust: Cargo check"
+cargo check --all-targets
+
+echo "+++ :clippy: Clippy"
+cargo clippy --all-targets -- -D warnings
